cd4bbad505ea03c32a948446d08a7d5042e3baac
harold <ananbbnn@gmail.com>
2025-04-23 21:40:14 +0800

    第一次用flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from datetime import datetime
+import pandas as pd
+import pymysql
+import requests
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    #return f'<h1>Hello World!</h1><br>{datetime.now()}'
+    username = 'Harold'
+    nowtime = datetime.now().strftime("%Y-%m-%d")
+    print(username,nowtime)
+    return render_template('index.html',name=username,now=nowtime)
+
+@app.route('/pm25-data')
+def get_pm25_data():
+    api_url = 'https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV'
+    response = requests.get(api_url, verify=False)
+    df = pd.read_csv(io.StringIO(response.text))
+    df['datacreationdate'] = pd.to_datetime(df['datacreationdate'])
+    df1 = df.dropna()
+    return df1.values.tolist()
+
+
+
+
+
+app.run(debug=True)
